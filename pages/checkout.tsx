4784cf610f0fc8f7d87df26d8a18b1dab4ceacbc
Or Druker <or@druker.net>
2022-09-27 14:56:14 +0300

    add Stripe checkout integration

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -8,6 +8,9 @@ import { useEffect, useState } from "react";
 import { CheckoutProduct } from "../components/CheckoutProduct";
 import Currency from "react-currency-formatter";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import Stripe from "stripe";
+import { fetchPostJSON } from "../utils/api-helpers";
+import getStripe from "../utils/get-stripejs";
 
 export const Checkout = () => {
   const items = useSelector(selectBasketItems);
@@ -16,9 +19,9 @@ export const Checkout = () => {
   const [groupedItemsInBasket, setGroupedItemsInBasket] = useState(
     {} as { [key: string]: Product[] }
   );
+  const [loading, setLoading] = useState(false);
 
   // Grouping the items into one array
-
   useEffect(() => {
     const groupedItems = items.reduce((results, item) => {
       (results[item._id] = results[item._id] || []).push(item);
@@ -27,6 +30,35 @@ export const Checkout = () => {
 
     setGroupedItemsInBasket(groupedItems);
   }, [items]);
+
+  const createCheckoutSession = async () => {
+    setLoading(true);
+    const checkoutSession: Stripe.Checkout.Session = await fetchPostJSON(
+      "/api/checkout_session",
+      {
+        items: items,
+      }
+    );
+
+    // Internal Server Error
+    if ((checkoutSession as any).statusCode === 500) {
+      console.error((checkoutSession as any).message);
+      return;
+    }
+    // Redirect to check out
+    const stripe = await getStripe();
+    const { error } = await stripe!.redirectToCheckout({
+      // Make the id field from the Checkout Session creation API response
+      // available to this file, so you can provide it as parameter here
+      // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+      sessionId: checkoutSession.id,
+    });
+    // If `redirectToCheckout` fails due to a browser or network
+    // error, display the localized error message to your customer
+    // using `error.message`.
+    console.warn(error.message);
+    setLoading(false);
+  };
 
   return (
     <div className="min-h-screen overflow-hidden bg-[#E7ECEE]">
@@ -96,7 +128,13 @@ export const Checkout = () => {
                   <h4 className="mb-4 flex flex-col text-xl font-semibold">
                     Proceed to Checkout
                   </h4>
-                  <Button title="Check Out" width="w-2/3" />
+                  <Button
+                    noIcon
+                    loading={loading}
+                    title="Check Out"
+                    width="w-2/3"
+                    onClick={createCheckoutSession}
+                  />
                 </div>
               </div>
             </div>
