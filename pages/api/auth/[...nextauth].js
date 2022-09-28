75733a6f60e8d208ba1fe78a7c562ca178feffdf
Or Druker <or@druker.net>
2022-09-28 16:59:03 +0300

    add next-auth with google provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
