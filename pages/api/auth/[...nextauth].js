be3b1ea3ec6ca86124b154ef784e6f4aead0ff9c
Or Druker <or@druker.net>
2022-09-28 16:40:33 +0300

    add auth data to sanity

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,21 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/google";
-export const authOptions = {
+import GoogleProvider from "next-auth/providers/google";
+import { SanityAdapter } from "next-auth-sanity";
+import { sanityClient } from "../../../sanity";
+
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
-};
-export default NextAuth(authOptions);
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: SanityAdapter(sanityClient),
+  debug: true,
+});
